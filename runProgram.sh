9383963eda5191201de986983cc0919b39436765
JerryFan225 <85361997+JerryFan225@users.noreply.github.com>
2023-05-30 17:13:55 +0100

    Colour and Label Update

--- runProgram.sh
+++ runProgram.sh
@@ -5,4 +5,4 @@ zenity --info --text "Rubbish Index:\nPlastic Bottle - <span foreground='green'>
 
 #Loads the trained model into detectnet and creates the video feed through which the detection is shown
 #Note that /dev/video0 here will only work for v4l2(usb) cameras and for any csi cameras, that part of the command will have to be changed to "csi://0"
-detectnet --model=/home/nvidia/rubbish_sorter/models/ssd-mobilenet.onnx --labels=/home/nvidia/rubbish_sorter/models/labels.txt --input-blob=input_0 --output-cvg=scores --output-bbox=boxes /dev/video0
+detectnet --model=/home/nvidia/rubbish_sorter/models/ssd-mobilenet.onnx --labels=/home/nvidia/rubbish_sorter/models/labels.txt --input-blob=input_0 --output-cvg=scores --output-bbox=boxes --colors=/home/nvidia/rubbish_sorter/models/colours.txt /dev/video0
